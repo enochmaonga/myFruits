e9b45299fdb15506a0c482dbbe4f75f25a40ec6e
enochmaonga <enoch.mokua@student.moringaschool.com>
2022-05-02 10:31:03 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+package fruits;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String[] myFruits = {"oranges", "Bananas", "Mangoes", "Pineaples", "Apples"};
+        Integer[] myFruitsPrices ={50,30,100,150,100};
+//        System.out.println(myFruits[1]);
+//        System.out.println(myFruits[0]);
+//        System.out.println(myFruits[4]);
+for(String myFruitsItem : myFruits){
+    System.out.println(myFruitsItem);
+
+}
+    Integer total = 0;
+    for(Integer price : myFruitsPrices){
+        total = total + price;
+    }
+        System.out.println("Dear customer, your fruits will cost Kshs "+ total +"/=");
+    }
+}
